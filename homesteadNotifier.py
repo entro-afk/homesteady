054067a2e753efb89f8f0dab722d346f82dcb922
Ana Calabano <acalabano@circleblack.com>
2020-04-28 17:54:09 -0400

    hopefully not breaking notifier

--- homesteadNotifier.py
+++ homesteadNotifier.py
@@ -44,7 +44,6 @@ async def check_if_reminder_needed():
                     )
                 )
                 conn.execute(delete_entry)
-                conn.commit()
     db.dispose()
 
 client.run(homesteadyConf['bot_token'])
